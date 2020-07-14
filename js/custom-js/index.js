85d93f13f9c6511a640c86fe5ad50f71b7df7996
Samuel Sholademi <52519811+Samuel377@users.noreply.github.com>
2020-07-14 13:44:38 +0100

    added animation

--- js/custom-js/index.js
+++ js/custom-js/index.js
@@ -40,6 +40,38 @@ function getTimeRemaining(endtime) {
   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
   initializeClock('clockdiv', deadline);
 
+window.sr = ScrollReveal();
+
+sr.reveal('.logo-left', {
+  duration: 2000,
+  origin: 'right',
+  distance: '200px'
+});
+sr.reveal('.logo-right', {
+  duration: 4000,
+  origin: 'left',
+  distance: '200px'
+});
+sr.reveal('.launch', {
+  duration: 3000,
+  origin: 'bottom',
+  distance: '300px'
+});
+sr.reveal('.email', {
+  duration: 3000,
+  origin: 'left',
+  distance: '300px'
+})
+sr.reveal('.submit-button', {
+  duration: 3000,
+  origin: 'right',
+  distance: '300px'
+})
+
+  function change() {
+    document.getElementsByClassName('landpage').style.background = "black";
+    console.log('right');
+  }
 
 
   document.getElementById('postForm').addEventListener('submit', postEmail);
