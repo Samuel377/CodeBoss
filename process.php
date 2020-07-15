ec4eedfc5f77c261a03db36b38b84bdea2957786
Samuel Sholademi <52519811+Samuel377@users.noreply.github.com>
2020-07-15 12:17:20 +0100

    Update process.php

--- process.php
+++ process.php
@@ -7,7 +7,7 @@ if (isset($_POST['email'])) {
     # code...
     // echo 'POST: Your email is '. $_POST['email'];
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $query = "INSERT INTO subscribers(email) VALUES('$email')";
+    $query = "INSERT INTO VALUES('$email')";
     if (mysqli_query($conn, $query)) {
         # code...
         echo 'User Added...';
