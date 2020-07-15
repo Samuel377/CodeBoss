b4f117f7127bf0ba46e169d9d61e50909f20715d
Samuel Sholademi <52519811+Samuel377@users.noreply.github.com>
2020-07-15 12:31:47 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,26 +1,19 @@
 <?php
-$servername = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "mlz2y2trsxppp424";
-$password = "xf080icpf8q4qpip";
-$dbname = "dr5umle9airr4q7s";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+//Connect to the database
+$conn = mysqli_connect('localhost', 'root', '', 'subscribe');
+
+if (isset($_POST['email'])) {
+    # code...
+    // echo 'POST: Your email is '. $_POST['email'];
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $query = "INSERT INTO subscribers(email) VALUES('$email')";
+    if (mysqli_query($conn, $query)) {
+        # code...
+        echo 'User Added...';
+    }else {
+        
+        echo 'Error: '.mysqli_error($conn);
+    }
 }
-if (isset($_POST['email'])){
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $sql = "INSERT INTO 3306(email)
-        VALUES ('$email')";
-
-        if ($conn->query($sql) === TRUE) {
-          echo "New record created successfully";
-        } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-}
-
-$conn->close();
 ?>
