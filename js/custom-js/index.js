e8d0eec9f98dcd055e792c861b4e49699140e23c
Samuel Sholademi <52519811+Samuel377@users.noreply.github.com>
2020-07-17 00:04:01 +0100

    new

--- js/custom-js/index.js
+++ js/custom-js/index.js
@@ -1,46 +1,4 @@
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-  
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-  
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
-    }
-  
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
-  
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
-
-window.sr = ScrollReveal();
+ window.sr = ScrollReveal();
 
 sr.reveal('.logo-left', {
   duration: 2000,
@@ -52,20 +10,45 @@ sr.reveal('.logo-right', {
   origin: 'left',
   distance: '200px'
 });
-sr.reveal('.launch', {
+sr.reveal('.text', {
   duration: 3000,
   origin: 'bottom',
   distance: '300px'
 });
-sr.reveal('.email', {
+sr.reveal('h2', {
   duration: 3000,
   origin: 'left',
   distance: '300px'
-})
-sr.reveal('.submit-button', {
+});
+sr.reveal('.how-it-works', {
+  duration: 3000,
+  origin: 'left',
+  distance: '300px'
+});
+sr.reveal('h5', {
   duration: 3000,
   origin: 'right',
   distance: '300px'
+});
+sr.reveal('h4', {
+  duration: 4000,
+  origin: 'top ',
+  distance: '300px'
+});
+sr.reveal('p', {
+  duration: 3000,
+  origin: 'left',
+  distance: '300px'
+});
+sr.reveal('.email', {
+  duration: 5000,
+  origin: 'left',
+  distance: '500px'
+});
+sr.reveal('.submit-button', {
+  duration: 5000,
+  origin: 'right',
+  distance: '500px'
 })
 
   function change() {
